9b8e510484162a085c0a2455b5348f4721331654
Kirill Velichko <cirill@live.ru>
2024-10-29 22:18:08 +0300

    Stash done

--- stash/stash/main.cpp
+++ stash/stash/main.cpp
@@ -6,7 +6,8 @@
 #include <fstream>
 #include <stdexcept>
 
-std::fstream OpenFile(const std::string& userPath, std::error_code& error_code){
+std::fstream OpenFile(const std::string& userPath){
+    std::error_code error_code;
     const std::filesystem::path path(userPath);
     if (!std::filesystem::is_regular_file(path, error_code)) {
         throw std::invalid_argument("Is not regular file");
@@ -36,17 +37,40 @@ void WriteFileType(std::fstream& file, std::span<unsigned char> data){
     }
 }
 
-int main()
+void ChangeMagicNumber(const std::string& command, const std::string& path, const std::string& password){
+    std::error_code error_code;
+    auto fileStream = OpenFile(path);
+    auto fileType = ReadFileType(fileStream);
+    std::span<unsigned char> fileTypeSpan((unsigned char*)fileType.data(), fileType.size());
+    std::vector<unsigned char> data;
+    if(command == "encrypt"){
+        data = gammagenerator::encrypt(password, fileTypeSpan);
+    } else{
+        data = gammagenerator::decrypt(password, fileTypeSpan);
+    }
+    WriteFileType(fileStream, std::span<unsigned char>(data.data(), data.size()));
+}
+
+int main(int argc, char* argv[])
 {
-    std::string password = "Danila crazy!";
-    std::uint64_t number = 123431325;
-    std::string input = "Some interesing data";
-    std::span inputSpan((unsigned char*)&number, sizeof(number));
-    auto encrypted = gammagenerator::encrypt(password, inputSpan);
-    std::span encrSpan(encrypted.data(), encrypted.size());
-    std::cout << std::string(encrSpan.begin(), encrSpan.end()) << std::endl;
-    auto result = gammagenerator::decrypt(password, encrSpan);
-    std::uint64_t outputNumber = *((std::uint64_t*)result.data());
-    std::string output(result.begin(), result.end());
-    std::cout << outputNumber << std::endl;
+    int returnCode = 0;
+    if(argc == 3){
+        std::string command(argv[1]);
+        std::string path(argv[2]);
+        if(command == "encrypt" || command == "decrypt"){
+            std::cout << "Please enter password" << std::endl;
+            std::string password;
+            std::cin >> password;
+            ChangeMagicNumber(command, path, password);
+            std::cout << "Successfully " << command << "ed" << std::endl;
+
+        } else{
+            std::cout << "Uncorrect command " << command << std::endl;
+            returnCode = 1;
+        }
+    } else{
+        std::cout << "Uncorrect args count" << std::endl;
+        returnCode = 1;
+    }
+    return returnCode;
 }
